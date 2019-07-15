83d30cb501c8b9acae4c24546bdd73646045c203
Irene Cordova <irene_cordova@outlook.es>
2019-07-15 05:13:44 -0500

    [ADD] Cambio de sentido del jugador al caminar hacia la izquiera o derecha

--- Proyecto/Assets/PlayerController.cs
+++ Proyecto/Assets/PlayerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D body;
+    private Animator animator;
+    public float walkingSpeed;
+  
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var v = body.velocity;
+        var speed = 0f;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            speed += -walkingSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            speed += +walkingSpeed;
+        }
+
+        v.x = speed;
+        body.velocity = v;
+
+        animator.SetFloat("moveSpeed", Mathf.Abs(speed) );
     }
+
 }
