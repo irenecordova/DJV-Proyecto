ffce30fec82f1109995149b8cbd90ffb0f1a9cfa
llullanqui <luis.1142@hotmail.com>
2019-07-21 14:55:08 -0500

    [UPD] PlayerController.cs: Métodos addPoints, substractLife

--- Proyecto/Assets/PlayerController.cs
+++ Proyecto/Assets/PlayerController.cs
@@ -7,11 +7,19 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D body;
     private Animator animator;
     public float walkingSpeed;
+    private int lifes;
+    private int totalPoints;
+    private int bonusPoints;
+    private boolean alive;
   
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        this.lifes = 3;
+        this.totalPoints = 0;
+        this.bonusPoints = 0;
+        this.alive = true;
     }
 
     // Update is called once per frame
@@ -33,6 +41,22 @@ public class PlayerController : MonoBehaviour
         body.velocity = v;
 
         animator.SetFloat("moveSpeed", Mathf.Abs(speed) );
+    }
+
+    void addPoints(int points) 
+    {
+        this.totalPoints += points;
+        this.bonusPoints += points;
+        while (this.bonusPoints >= 50000)
+        {
+            this.lifes += 1;
+            this.bonusPoints -= 50000;
+        }
+    }
+
+    void substractLife() {
+        this.lifes -= 1;
+        this.alive = this.lifes != 0;
     }
 
 }
