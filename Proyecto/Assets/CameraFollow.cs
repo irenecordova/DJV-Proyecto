64a7ffda88d2f99e363e9b13a211e19ea600b7e6
Andrea Jurado <avjh_18@hotmail.com>
2019-07-22 00:29:38 -0500

    Follow camera

---
+++ Proyecto/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject character;
+    private Vector3 position;
+    void Start()
+    {
+        position =transform.position - character.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = character.transform.position + position;   
+    }
+}
