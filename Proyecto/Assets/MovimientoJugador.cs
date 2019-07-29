66322a589359cbdc2f7bb496be6244a21a5fdef5
llullanqui <luis.1142@hotmail.com>
2019-07-28 20:30:05 -0500

    Nuevos assets, tiles... Nuevo todo :c

---
+++ Proyecto/Assets/MovimientoJugador.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoJugador : MonoBehaviour
+{
+
+    private int lifes;
+    private int totalPoints;
+    private int bonusPoints;
+    private bool alive;
+    private double movementSpeed;
+    private double jumpSpeed;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        this.lifes = 3;
+        this.totalPoints = 0;
+        this.bonusPoints = 0;
+        this.alive = true;
+        this.movementSpeed = 0.085;
+        this.jumpSpeed = 10;
+        this.rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)){
+            Vector3 position = this.transform.position;
+            position.x = position.x + (float)this.movementSpeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            Vector3 position = this.transform.position;
+            position.x = position.x - (float)this.movementSpeed;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow)){
+            Jump();
+        }
+    }
+
+    void Jump() {
+        this.rb.AddForce(new Vector2(0,(float)this.jumpSpeed), ForceMode2D.Impulse);
+    }
+}
