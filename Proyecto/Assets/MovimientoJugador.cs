fa235ec5130bad6f91628addca81f278da5e99d5
llullanqui <luis.1142@hotmail.com>
2019-07-28 23:10:16 -0500

    Animaciones de movimiento de player macho

--- Proyecto/Assets/MovimientoJugador.cs
+++ Proyecto/Assets/MovimientoJugador.cs
@@ -9,9 +9,11 @@ public class MovimientoJugador : MonoBehaviour
     private int totalPoints;
     private int bonusPoints;
     private bool alive;
+    private bool moving;
     private double movementSpeed;
     private double jumpSpeed;
     private Rigidbody2D rb;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start() {
@@ -22,15 +24,35 @@ public class MovimientoJugador : MonoBehaviour
         this.movementSpeed = 0.085;
         this.jumpSpeed = 10;
         this.rb = GetComponent<Rigidbody2D>();
+        this.animator = GetComponent<Animator>();
+        this.moving = false;
     }
 
     // Update is called once per frame
     void Update() {
+        if (Input.GetKeyDown(KeyCode.RightArrow)){
+            //this.animator.ResetTrigger("Walking Left");
+            this.animator.SetTrigger("Walking Right");
+            this.moving = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)){
+            //this.animator.ResetTrigger("Walking Right");
+            this.animator.SetTrigger("Walking Left");
+            this.moving = true;
+        }
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)){
+            //this.animator.ResetTrigger("Walking Right");
+            if (this.moving) {
+                this.animator.SetTrigger("Idle");
+                this.moving = false;
+            }
+        }
         if (Input.GetKey(KeyCode.RightArrow)){
             Vector3 position = this.transform.position;
             position.x = position.x + (float)this.movementSpeed;
             this.transform.position = position;
         }
+        
         if (Input.GetKey(KeyCode.LeftArrow)){
             Vector3 position = this.transform.position;
             position.x = position.x - (float)this.movementSpeed;
