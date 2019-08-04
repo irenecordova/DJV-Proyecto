490f01828a2d31a043a80641675b51ebc368a5ee
Irene Cordova <irene_cordova@outlook.es>
2019-08-04 13:00:40 -0500

    [FIX] Jump

--- Proyecto/Assets/Scripts/MovimientoJugador.cs
+++ Proyecto/Assets/Scripts/MovimientoJugador.cs
@@ -11,9 +11,10 @@ public class MovimientoJugador : MonoBehaviour
     private bool alive;
     private bool moving;
     private double movementSpeed;
-    private double jumpSpeed;
+    private float jumpSpeed;
     private Rigidbody2D rb;
     private Animator animator;
+    public bool grounded;
 
     // Start is called before the first frame update
     void Start() {
@@ -22,48 +23,79 @@ public class MovimientoJugador : MonoBehaviour
         this.bonusPoints = 0;
         this.alive = true;
         this.movementSpeed = 0.085;
-        this.jumpSpeed = 10;
+        this.jumpSpeed = 17f;
         this.rb = GetComponent<Rigidbody2D>();
         this.animator = GetComponent<Animator>();
         this.moving = false;
+        this.grounded = true;
     }
 
     // Update is called once per frame
-    void Update() {
-        if (Input.GetKeyDown(KeyCode.RightArrow) && Time.timeScale != 0){
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) && Time.timeScale != 0)
+        {
             //this.animator.ResetTrigger("Walking Left");
             this.animator.SetTrigger("Walking Right");
             this.moving = true;
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && Time.timeScale != 0){
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && Time.timeScale != 0)
+        {
             //this.animator.ResetTrigger("Walking Right");
             this.animator.SetTrigger("Walking Left");
             this.moving = true;
         }
-        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow)){
+        if (!Input.GetKey(KeyCode.RightArrow) && !Input.GetKey(KeyCode.LeftArrow))
+        {
             //this.animator.ResetTrigger("Walking Right");
-            if (this.moving) {
+            if (this.moving)
+            {
                 this.animator.SetTrigger("Idle");
                 this.moving = false;
             }
         }
-        if (Input.GetKey(KeyCode.RightArrow) && Time.timeScale != 0){
+        if (Input.GetKey(KeyCode.RightArrow) && Time.timeScale != 0)
+        {
             Vector3 position = this.transform.position;
             position.x = position.x + (float)this.movementSpeed;
             this.transform.position = position;
         }
         
-        if (Input.GetKey(KeyCode.LeftArrow) && Time.timeScale != 0){
+        if (Input.GetKey(KeyCode.LeftArrow) && Time.timeScale != 0)
+        {
             Vector3 position = this.transform.position;
             position.x = position.x - (float)this.movementSpeed;
             this.transform.position = position;
         }
-        if (Input.GetKeyDown(KeyCode.UpArrow)){
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
+        {
+            this.rb.velocity = new Vector2(this.rb.velocity.x, 0);
             Jump();
         }
     }
 
-    void Jump() {
-        this.rb.AddForce(new Vector2(0,(float)this.jumpSpeed), ForceMode2D.Impulse);
+    void Jump()
+    {
+        this.rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+    }
+
+    void OnCollisionStay2D(Collision2D col)
+    {
+        var otherObject = col.collider.gameObject;
+
+        if (otherObject.tag == "Floor")
+        {
+           grounded = true;
+        }
+    }
+
+     void OnCollisionExit2D(Collision2D col)
+    {
+        var otherObject = col.collider.gameObject;
+
+        if (otherObject.tag == "Floor")
+        {
+           grounded = false;
+        }
     }
 }
