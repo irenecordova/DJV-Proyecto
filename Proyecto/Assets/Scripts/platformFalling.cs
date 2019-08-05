16bb31aa14b7821d685866b8e3d96a3774b5a226
Andrea Jurado <avjh_18@hotmail.com>
2019-08-04 23:09:17 -0500

    platforms

---
+++ Proyecto/Assets/Scripts/platformFalling.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformFalling : MonoBehaviour{
+    private Rigidbody2D platform;
+    private PolygonCollider2D polygon;
+    void Start()
+    {
+        platform = GetComponent<Rigidbody2D>();
+        polygon = GetComponent<PolygonCollider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnClollisionEnter2d(Collision2D col){
+
+        if(col.gameObject.CompareTag("Player")){
+            platform.isKinematic = false;
+            polygon.isTrigger = true;
+        }
+    }
+}
+
+
---
+++ Proyecto/Assets/Scripts/timerControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerControl : MonoBehaviour
+{
+    public GUIText timeText;
+    public float  time = 0.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time = time++ * Time.deltaTime;
+        timeText.text = "" + time.ToString("f0");
+
+    }
+}
