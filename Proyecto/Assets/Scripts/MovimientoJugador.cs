4fe7f60734e45623520f6bcde4e93fbd4654497b
llullanqui <luis.1142@hotmail.com>
2019-08-05 00:36:06 -0500

    [ADD] Hammer interaction

--- Proyecto/Assets/Scripts/MovimientoJugador.cs
+++ Proyecto/Assets/Scripts/MovimientoJugador.cs
@@ -24,7 +24,6 @@ public class MovimientoJugador : MonoBehaviour
         this.grounded = true;
         this.climbing = false;
         this.objects = new List<string>();
-        this.objects.Add("martillo");
     }
 
     // Update is called once per frame
---
+++ Proyecto/Assets/Scripts/usableObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class usableObject : MonoBehaviour
+{
+    public string nombre;
+    private GameObject player;
+    private MovimientoJugador scriptPlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.player = GameObject.Find("MalePlayer");
+        this.scriptPlayer = (MovimientoJugador) this.player.GetComponent(typeof(MovimientoJugador));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            this.scriptPlayer.getObjectList().Add(this.nombre);
+            Destroy(gameObject);
+        }
+    }
+    
+}
