5ca8666be4f68dd3a71dbe6635635922f3c1e82c
llullanqui <luis.1142@hotmail.com>
2019-08-04 21:40:49 -0500

    [ADD] Destroy wall by pressing key

---
+++ Proyecto/Assets/Scripts/CheckIfPlayerIsAtLeft.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckIfPlayerIsAtLeft : MonoBehaviour
+{
+    private bool destroyable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.destroyable = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.destroyable && Input.GetKeyDown(KeyCode.X)) {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            this.destroyable = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (other.gameObject.tag == "Player") {
+            this.destroyable = false;
+        }
+    }
+}
