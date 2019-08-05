947a237f01dba83792acbf3c4683cebbc524b5bf
Irene Cordova <irene_cordova@outlook.es>
2019-08-05 04:16:24 -0500

    [ADD] Timer and game over

---
+++ Proyecto/Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float time = 0.0f;
+    public Text txt;
+    public Canvas canvas;
+    // Start is called before the first frame update
+    void Start()
+    {
+        /* Canvas canvas = CanvasGameOver.GetComponent<Canvas>(); */
+        canvas.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            canvas.enabled = true;
+            Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        }
+        else
+        {
+            txt.GetComponent<Text>().text = "" + time.ToString("f0");
+        }
+        
+
+        
+    }
+}
