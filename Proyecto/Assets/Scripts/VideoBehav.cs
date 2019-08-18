3bd13f233eeda3dc988077ccb5b1b94ba77be4da
Gloria Alcivar <sukkie_hye501@hotmail.com>
2019-08-17 23:52:55 -0500

    videoplayer

---
+++ Proyecto/Assets/Scripts/VideoBehav.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoBehav : MonoBehaviour
+{
+
+    public VideoPlayer vp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            vp.Play();
+            }
+}
